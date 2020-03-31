168a035c146fde946a69f64422e3e61e976ad90f
Wolgrand Pinto Lordao Neto <wolgrand.lordao@energisa.com.br>
2020-03-31 16:10:35 -0300

    Fix x-total-count

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -1,23 +1,22 @@
-const connection = require("../database/connection");
+const connection = require('../database/connection');
 
 module.exports = {
   async index(req, res) {
     const { page = 1 } = req.query;
 
-    const [count] = await connection("incidents").count();
-    res.header("X-Total-Count", count["count"]);
-
-    const incidents = await connection("incidents")
-      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+    const [count] = await connection('incidents').count();
+    res.header('X-Total-Count', count['count(*)']);
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
       .select([
-        "incidents.*",
-        "ongs.name",
-        "ongs.email",
-        "ongs.whatsapp",
-        "ongs.city",
-        "ongs.uf"
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf'
       ]);
 
     return res.json(incidents);
@@ -27,7 +26,7 @@ module.exports = {
 
     const ong_id = req.headers.authorization;
 
-    const [id] = await connection("incidents").insert({
+    const [id] = await connection('incidents').insert({
       title,
       description,
       value,
@@ -41,17 +40,17 @@ module.exports = {
     const { id } = req.params;
     const ong_id = req.headers.authorization;
 
-    const incident = await connection("incidents")
-      .where("id", id)
-      .select("ong_id")
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
       .first();
 
     if (incident.ong_id !== ong_id) {
-      return res.status(401).json({ error: "Operation not permitted." });
+      return res.status(401).json({ error: 'Operation not permitted.' });
     }
 
-    await connection("incidents")
-      .where("id", id)
+    await connection('incidents')
+      .where('id', id)
       .delete();
 
     return res.status(204).send();
