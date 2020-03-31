443fe85beefbab54996c154cb1c98f0f107914fd
Wolgrand Pinto Lordao Neto <wolgrand.lordao@energisa.com.br>
2020-03-31 10:22:34 -0300

    Fix duplicated response

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -5,21 +5,20 @@ module.exports = {
     const { page = 1 } = req.query;
 
     const [count] = await connection("incidents").count();
+    res.header("X-Total-Count", count["count"]);
 
     const incidents = await connection("incidents")
-      .join("ongs", "ong_id", "=", "incidents.ong_id")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
       .limit(5)
       .offset((page - 1) * 5)
-      .select(
+      .select([
         "incidents.*",
         "ongs.name",
         "ongs.email",
         "ongs.whatsapp",
         "ongs.city",
         "ongs.uf"
-      );
-
-    res.header("X-Total-Count", count["count(*)"]);
+      ]);
 
     return res.json(incidents);
   },
